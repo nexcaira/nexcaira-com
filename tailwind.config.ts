c84e6551c9c5f5660b7eef82191a742c21033cca
aenjojo <josuafernando999@gmail.com>
2024-12-24 16:27:04 +0700

    chore: setup custom font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
