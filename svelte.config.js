c6f4fd5998e1607eb7215d62abf86383425f3c8f
aenjojo <josuafernando999@gmail.com>
2024-12-24 16:26:21 +0700

    chore: use bun adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
-  }
+    adapter: adapter(),
+  },
 };
 
 export default config;
