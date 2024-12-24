0624a3415759c1e13d8dbb5958672e722b3e2095
aenjojo <josuafernando999@gmail.com>
2024-12-24 16:28:28 +0700

    feat: update main page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  // 
+</script>
+
+<div class="w-full h-screen bg-slate-900 grid place-content-center">
+  <div class="">
+    <h1 class="text-4xl lg:text-8xl font-bold">
+      <span class="text-amber-500">Nex</span>
+      <span class="text-sky-500">Caira</span>
+      <span class="text-emerald-500">Devel</span>
+    </h1>
+  </div>
+</div>
+
+<style lang="postcss">
+  /*  */
+</style>
